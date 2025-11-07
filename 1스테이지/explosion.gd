c4719ead5c5e1edf277fbbfb93efc779111dabed
jimispace <woojoo3268@gmail.com>
2025-11-07 18:41:41 +0900

    Add files via upload

---
+++ 1스테이지/explosion.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var animated_explosion = $AnimatedExplosion # AnimatedSprite2D 노드의 이름
+
+func _ready():
+	# 애니메이션 재생이 끝나면 _on_animation_finished 함수를 호출하도록 연결
+	animated_explosion.animation_finished.connect(_on_animation_finished)
+	animated_explosion.play("explode") # 또는 설정한 애니메이션 이름 (예: "explode")
+
+func _on_animation_finished():
+	# 애니메이션이 끝나면 이 씬(폭발)을 제거합니다.
+	queue_free()
